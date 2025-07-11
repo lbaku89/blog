6b0082dc5a1457d7b09b39fb8f5ed10dfeb381fe
hyunwoo <khj930410@naver.com>
2025-07-11 21:55:06 +0900

    chore: install tailwind-animate , setting done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,7 @@ const config: Config = {
     './component/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  plugins: [require('tailwindcss-animated')],
   theme: {
     extend: {
       colors: {
