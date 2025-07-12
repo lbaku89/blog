9ee25011963d6e180cef26cd928da94cd4601e27
hyunwoo <khj930410@naver.com>
2025-07-12 21:32:45 +0900

    feat: MDXRemote 에서 사용하기 위한 컴포넌트 리스트 작성 유틸 작성

---
+++ utils/mdx-components.ts
@@ -0,0 +1,8 @@
+import { ProgressBar, ProgressBar2, ProgressBar3 } from '@/component/ProgressBar'
+
+export const mdxComponents = {
+  ProgressBar,
+  ProgressBar2,
+  ProgressBar3,
+  // 필요한 다른 컴포넌트들 추가 가능
+}
