987d45ba34619ec42dcf939221f918ffa2d4711a
hyunwoo <khj930410@naver.com>
2025-07-13 11:19:17 +0900

    chore: 확장자 명 변경

--- utils/mdx-components.ts
+++
@@ -1,8 +0,0 @@
-import { ProgressBar, ProgressBar2, ProgressBar3 } from '@/component/ProgressBar'
-
-export const mdxComponents = {
-  ProgressBar,
-  ProgressBar2,
-  ProgressBar3,
-  // 필요한 다른 컴포넌트들 추가 가능
-}
