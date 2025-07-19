a9ea625d83ce08f19e77c2e1c69aaf84aabe3e80
hyunwoo <khj930410@naver.com>
2025-07-20 00:21:54 +0900

    feat: table of contents ( 목차 ), autoLInk 기능 추가 완료

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -1,4 +1,4 @@
-import { getAllPosts, getPostBySlug } from '@/utils/post'
+import { getAllPosts, getPostBySlug, getTocFromMdx } from '@/utils/post'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import { mdxComponents } from '@/mdx-components'
 import { Badge } from '@/component/Badge'
@@ -17,6 +17,7 @@ export default async function Page({ params }: { params: Promise<{ slug: string[
 
   // 개별 포스트만 조회 (더 효율적)
   const currentPost = await getPostBySlug(slugPath)
+  const { tocHtml } = await getTocFromMdx(currentPost?.body || '')
 
   if (!currentPost) {
     return <div>Post not found</div>
@@ -24,50 +25,68 @@ export default async function Page({ params }: { params: Promise<{ slug: string[
 
   return (
     <article>
-      {/* 포스트 제목, 설명, 태그 등 */}
-      <div className="flex flex-col gap-4">
-        <h1 className="text-center">{currentPost.frontMatter.title}</h1>
-        <div className="flex flex-col gap-2">
-          <h5 className="text-center">{currentPost.frontMatter.description}</h5>
-          <div className="flex items-center justify-center gap-2">
-            <CalendarIcon className="size-5 text-blue-600"></CalendarIcon>
-            <p className="text-center">{getYYYYMMDD(currentPost.frontMatter.date)}</p>
-          </div>
-          <ul className="mb-4 flex gap-2 justify-center list-none">
-            {currentPost.frontMatter.tags.map((tag) => (
-              <li key={tag}>
-                <Badge>{tag}</Badge>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
       {/* 컴포넌트 맵을 전달 */}
-      <div className="p-2 prose  dark:prose-invert max-w-none">
-        <MDXRemote
-          source={currentPost.body}
-          components={mdxComponents}
-          options={{
-            mdxOptions: {
-              remarkPlugins: [remarkGfm],
-              rehypePlugins: [
-                [
-                  rehypePrettyCode,
-                  {
-                    theme: 'night-owl',
-                  },
-                  rehypeSanitize,
-                  rehypeStringify,
-                  /**
-                   * @todo 하단 2개 플러그인 적용 확인 필요
-                   */
-                  rehypeAutolinkHeadings,
-                  rehypeSlug,
-                ],
-              ],
-            },
-          }}
-        />
+      <div className="flex gap-4">
+        {/* 포스트 헤더 + 포스팅 */}
+        <div>
+          {/* 포스트 헤더 제목, 설명, 태그 등 */}
+          <div className="flex flex-col gap-4">
+            <h1 className="text-center">{currentPost.frontMatter.title}</h1>
+            <div className="flex flex-col gap-2">
+              <h5 className="text-center">{currentPost.frontMatter.description}</h5>
+              <div className="flex items-center justify-center gap-2">
+                <CalendarIcon className="size-5 text-blue-600"></CalendarIcon>
+                <p className="text-center">{getYYYYMMDD(currentPost.frontMatter.date)}</p>
+              </div>
+              <ul className="mb-4 flex gap-2 justify-center list-none">
+                {currentPost.frontMatter.tags.map((tag) => (
+                  <li key={tag}>
+                    <Badge>{tag}</Badge>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          {/* 포스팅 내용 */}
+          <div className="prose dark:prose-invert max-w-none p-2">
+            <MDXRemote
+              source={currentPost.body}
+              components={mdxComponents}
+              options={{
+                mdxOptions: {
+                  remarkPlugins: [remarkGfm],
+                  rehypePlugins: [
+                    /** @description prevent xss */
+                    rehypeSanitize,
+                    /** @description rehype tree 를 최종 html로 변환 */
+                    rehypeStringify,
+                    [rehypePrettyCode, { theme: 'tokyo-night' }],
+
+                    /** @description add id to heading tag */
+                    rehypeSlug,
+                    [
+                      rehypeAutolinkHeadings,
+                      {
+                        behavior: 'wrap',
+                        properties: {
+                          className: ['no-underline hover:text-blue-700 dark:hover:text-blue-400'],
+                        },
+                      },
+                    ],
+                  ],
+                },
+              }}
+            />
+          </div>
+        </div>
+        {/* toc영역 */}
+        <div>
+          <div className="toc-container">
+            <h4 className="mb-[10px] text-gray-900 dark:text-gray-100">Table of Content</h4>
+            <div dangerouslySetInnerHTML={{ __html: tocHtml }} />
+          </div>
+        </div>
+        {/* </div> */}
       </div>
     </article>
   )
