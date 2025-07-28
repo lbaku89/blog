c4921e78458711c6169f370ef6b9698a1318a963
hyunwoo <khj930410@naver.com>
2025-07-28 20:57:12 +0900

    feat: add posting

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,8 +1,15 @@
 import { ProgressBar, ProgressBar2, ProgressBar3 } from '@/component/ProgressBar'
-
+import Image from 'next/image'
 export const mdxComponents = {
   ProgressBar,
   ProgressBar2,
   ProgressBar3,
+  Image: (props: React.ComponentPropsWithoutRef<typeof Image>) => {
+    return <Image {...props} />
+  },
+
   // 필요한 다른 컴포넌트들 추가 가능
+  // code: ({ className, ...props }: React.ComponentPropsWithoutRef<'code'>) => {
+  //   return <code className=" !bg-gray-500 !text-blue-400 !px-[3px] !py-[3px] !rounded-sm !mr-[3px]" {...props} />
+  // },
 }
