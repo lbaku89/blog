d02f489312cec18caa8d624112fd193fa124ed66
hyunwoo <khj930410@naver.com>
2025-07-28 23:28:35 +0900

    feat: add posting

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
       </head>
       <body className="flex flex-col min-h-screen">
         <div
-          className={`${geistSans.variable} ${geistMono.variable} antialiased w-full max-w-full lg:w-[1024px] mx-auto px-4`}
+          className={`${geistSans.variable} ${geistMono.variable} antialiased w-full max-w-full lg:w-[1024px] mx-auto px-4 flex-1`}
         >
           <Header />
           <div className="py-[30px] flex-1">{children}</div>
