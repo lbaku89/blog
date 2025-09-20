59894498a0a10d3dcfa2d8ac783f46ce4b3b485f
hyunwoo <khj930410@naver.com>
2025-09-20 18:05:07 +0900

    etc: title 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 })
 
 export const metadata: Metadata = {
-  title: 'Hyunwoo',
+  title: "Hyunwoo' Blog",
   description: 'Frontend 개발, Next.js, React, 그리고 기술 성장 이야기',
   keywords: ['Next.js', 'React', 'Frontend', '기술 블로그', 'Hyunwoo'],
   authors: [{ name: 'Hyunwoo Kwon', url: 'https://kwonhyunwoo.dev' }],
