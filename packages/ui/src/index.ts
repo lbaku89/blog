b7d8f03efae4133ba818a9e98151de19ba7a54a3
hyunwoo <khj930410@naver.com>
2026-01-06 01:15:39 +0900

    feat: @common-ui > Label Component 추가

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,17 @@
 export { Badge } from './components/badge/index'
 export { Button } from './components/button/index'
+export {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogTrigger,
+  DialogClose,
+} from './components/Dialog/index'
+export { Input } from './components/Input/index'
+export { Label } from './components/Label/index'
 
+// utils
 export { cn } from './lib/utils'
