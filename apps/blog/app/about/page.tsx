77a6932245eaaa3c8f36078e7c8d493af11711b8
hyunwoo <khj930410@naver.com>
2026-01-05 21:37:43 +0900

    etc: about me page contents 레이아웃 변경 (반응형 처리)

--- apps/blog/app/about/page.tsx
+++ apps/blog/app/about/page.tsx
@@ -5,15 +5,15 @@ const AboutPage = () => {
       <h1>About Me</h1>
       <hr className="my-[8px]" />
       <div className="grid grid-cols-3 gap-4">
-        <div className="col-span-1 relative h-[300px] flex flex-col items-center pt-[20px]">
+        <div className="col-span-3 md:col-span-1 relative h-[300px] flex flex-col items-center pt-[20px]">
           <Image src="/avatar.webp" alt="profile" width={200} height={200} className="rounded-full" />
           <p className="font-semibold py-2">Hyunwoo</p>
           <p>Frontend Developer</p>
           <span>Seoul, Korea</span>
         </div>
-        <div className="col-span-2 pt-[20px] flex flex-col gap-4">
+        <div className="col-span-3 md:col-span-2 pt-[20px] flex flex-col gap-4">
           <p>
-            안녕하세요, 4년차 프론트엔드 개발자 권현우입니다. <br />
+            안녕하세요, 프론트엔드 개발자 권현우입니다. <br />
             제가 짠 코드로 유용한 서비스,프로그램을 만들수 있다는 매력에 빠져 개발자로 일하고 있습니다.
           </p>
           <div>
