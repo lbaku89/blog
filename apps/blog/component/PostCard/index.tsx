531853bb76949c96b38ff4a0555f4d40c7c3b641
hyunwoo <khj930410@naver.com>
2026-09-08 17:04:07 +0900

    refactor: 포스트카드 컴포넌트 수정 ( 태그 클릭시 해당 태그 기준으로 포스트카드 검색 )

--- apps/blog/component/PostCard/index.tsx
+++ apps/blog/component/PostCard/index.tsx
@@ -13,22 +13,27 @@ export const PostCard = ({ post }: { post: Post }) => {
       <li>
         <a href={`/posts/${post.slug}`} className="block mb-4">
           <TypographyH3 className="">{post.frontMatter.title}</TypographyH3>
-
-          <ul className="flex flex-wrap gap-2 mt-3 list-none">
-            <TypographyP>{getYYYYMMDD(post.frontMatter.date)}</TypographyP>
-            {/* admin 전용 포스팅이면 "admin-only" 태그 추가 */}
-            {post.frontMatter.adminOnly && (
-              <li key="admin-only">
+        </a>
+        <ul className="flex flex-wrap gap-2 mt-3 list-none">
+          <TypographyP>{getYYYYMMDD(post.frontMatter.date)}</TypographyP>
+          {/* admin 전용 포스팅이면 "admin-only" 태그 추가 */}
+          {post.frontMatter.adminOnly && (
+            <li key="admin-only">
+              <a href="/?tag=admin-only" className="hover:underline">
                 <Badge variant="secondary">admin-only</Badge>
-              </li>
-            )}
-            {/* 일반 태그 표시 */}
-            {post.frontMatter.tags.map((tag) => (
-              <li key={tag}>
+              </a>
+            </li>
+          )}
+          {/* 일반 태그 표시 */}
+          {post.frontMatter.tags.map((tag) => (
+            <li key={tag}>
+              <a href={`/?tag=${encodeURIComponent(tag)}`} className="hover:underline">
                 <Badge variant="secondary">{tag}</Badge>
-              </li>
-            ))}
-          </ul>
+              </a>
+            </li>
+          ))}
+        </ul>
+        <a href={`/posts/${post.slug}`} className="block">
           <TypographyP className="mt-3">{post.frontMatter.description}</TypographyP>
         </a>
       </li>
